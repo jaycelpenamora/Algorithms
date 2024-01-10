7f3ec62ef9364eaceba2901e188ed18bbdae35a9
jaycelpenamora <jaycelpenamora@gmail.com>
2024-01-10 11:57:12 +0800

    modified:   euclid.cpp 	new file:   recursion.cpp

--- euclid.cpp
+++ euclid.cpp
@@ -1,5 +1,7 @@
+#include <array>
 #include <iostream>
 #include <string>
+<<<<<<< HEAD
 
 using std::string, std::cout, std::cin, std::endl;
 
@@ -9,6 +11,16 @@ private:
 
 public:
   ExceptionInvalidInput(int value) : y(value) {}
+=======
+using namespace std;
+
+class ExceptionInvalidInput : public std::exception {
+private:
+  long long y;
+
+public:
+  ExceptionInvalidInput(long long value) : y(value) {}
+>>>>>>> 2db0b99 (	modified:   compiled/euclid)
 
   const char *what() const noexcept override {
     std::string message = "Number nonpositive or zero: " + std::to_string(y);
@@ -16,9 +28,10 @@ public:
   }
 };
 
-int gcd(int x, int y);
+long long gcd(long x, long y);
 
 int main() {
+<<<<<<< HEAD
   int m, n, result;
 
   cout << "Enter two non-positive integers\n";
@@ -28,6 +41,17 @@ int main() {
       throw ExceptionInvalidInput(m);
     if (n <= 0)
       throw ExceptionInvalidInput(n);
+=======
+
+  long long m, n, result;
+  cout << "Enter two non-positive integers\n";
+  try {
+    cin >> m >> n;
+    if (m < 0 || m == 0)
+      throw ExceptionInvalidInput(m);
+    if (n < 0 || n == 0)
+      throw ExceptionInvalidInput(m);
+>>>>>>> 2db0b99 (	modified:   compiled/euclid)
   } catch (ExceptionInvalidInput &e) {
     cout << e.what() << endl;
   }
@@ -37,10 +61,17 @@ int main() {
 
   return 0;
 }
+<<<<<<< HEAD
 int gcd(int x, int y) {
   if (y == 0)
     return x;
   int r = x % y;
+=======
+long long gcd(long x, long y) {
+  if (y == 0)
+    return x;
+  long long r = x % y;
+>>>>>>> 2db0b99 (	modified:   compiled/euclid)
   cout << x << " = " << y << " * " << x / y << " + " << x % y << endl;
   return gcd(y, r);
 }
