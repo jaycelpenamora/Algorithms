064ad73abfaa45138d814219384c2a9941f9abb3
jaycelpenamora <jaycelpenamora@gmail.com>
2024-01-12 18:51:32 +0800

    new file:   recursion-add.cpp new file:   recursion-reverse.cpp

---
+++ recursion-add.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using std::cout, std::endl;
+int recurse(int *arr, int index, int size) {
+  if (index == size) {
+    return 0;
+  } else {
+    return arr[index] + recurse(arr, index + 1, size);
+  }
+}
+int main() {
+  int size{10};
+  int *arr = new int[size]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  cout << recurse(arr, 0, size) << endl;
+  delete[] arr;
+  return 0;
+}
